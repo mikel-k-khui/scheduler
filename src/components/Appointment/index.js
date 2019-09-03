06c573e48f86b8d1a192f4c678be0761776073f3
Mike Chui <mikel.k.khui@gmail.com>
2019-09-03 17:21:51 -0600

    Updated the compoents for Appointment except for Form

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import "./styles.scss"
+
+export default function Appointment(props) {
+  // const items = props.interviewers.map(interviewer => {
+  //   return (
+  //   <InterviewerListItem 
+  //     key={interviewer.id}
+  //     name={interviewer.name}
+  //     avatar={interviewer.avatar}
+  //     selected={interviewer.id === props.value}
+  //     setInterviewer={(event) => props.onChange(interviewer.id)}
+  //   />
+  //   );
+  // });
+
+  return (
+<article className="appointment">
+  
+</article>    );
+}
