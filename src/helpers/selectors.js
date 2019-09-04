62547d5662b4cf94c5a6c551619144d8f1afc1eb
Mike Chui <mikel.k.khui@gmail.com>
2019-09-04 11:27:17 -0600

    Created getInterview function in selectors with test from Jest

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,10 @@
 
+/**
+ * Render an array of appointments
+ * @state {Object} All data in state pulled from API.
+ * @day {String} currently seleted day
+ * @return [Array]
+ */
 export function getAppointmentsForDay(state, day) {
   const matched = [];
   let appArray = state.days.filter(aDay =>  aDay.name === day)
@@ -8,7 +14,26 @@ export function getAppointmentsForDay(state, day) {
       if (appArray.includes(state.appointments[app]["id"])) {
         matched.push(state.appointments[app]);
       }
+      return matched;
     });
   }
+  return matched;
+}
+
+/**
+ * Render an array of appointments
+ * @state {Object} All data in state pulled from API.
+ * @seekInterview {Object} with student name and interviewer number
+ * @return {Object} with interviewer information filled in
+ */
+export function getInterview(state, seekInterview) {
+  let matched = null;
+  if (seekInterview) {
+    let interviewers = state["interviewers"];
+    if (interviewers[seekInterview["interviewer"]]) {
+        seekInterview["interviewer"] = interviewers[seekInterview["interviewer"]];
+        matched = seekInterview;
+      }
+    }
   return matched;
 }
