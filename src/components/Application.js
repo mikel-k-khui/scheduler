24e910047edb88202fc9a13deffc0555b21d1766
Mike Chui <mikel.k.khui@gmail.com>
2019-09-03 23:06:56 -0600

    Updated the axios and useEffect to retrieve days data from API server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 // import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -80,6 +63,21 @@ const appScript = appointments.map(app => {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([{id: 1,
+        name: "Monday",
+        spots: 2,
+      }]);
+  
+  useEffect(() => {
+    console.log("Before get:");
+    axios
+      .get('/api/days')
+      .then(res => {
+        console.log("After get:", res.data);
+        setDays(res.data);
+      })
+      .catch(e => console.log("error", e));
+  }, []);
 
   return (
     <main className="layout">
