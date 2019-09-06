d730ad9d72943556641a52fc4de8c60cd6e250bf
Mike Chui <mikel.k.khui@gmail.com>
2019-09-06 02:09:37 -0600

    Added setSpots function to update available spots.  There is still no check to ensure no more than 5 appointments are booked.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,12 +16,27 @@ export default function useApplicationData(props) {
       case SET_DAY:
         return Object.assign({}, state, { day: action.day });
       case SET_APPLICATION_DATA:
-          const { type, ...rest} = action;
-          console.log(rest);
-          return Object.assign({}, state, { ...rest});
-      case SET_SPOTS: {
-        return /* insert logic */
-      }
+          let { type, ...any} = action;
+          return Object.assign({}, state, { ...any});
+      case SET_SPOTS: 
+        let { id, change} = action;
+        // console.log("In set spots:", id, "and change:", change);
+        // console.log("Which day to change:", state.days[Math.floor(id / 5)]);
+
+        const pos = Math.floor(id / 5);
+        // slice(0, pos);
+        // slice(pos + 1, .length);
+        let days = [...state["days"]];
+        let day = {...days[pos]};
+        const newSpots = Number(days[pos]["spots"]) + Number(change);
+        day = {...day, spots: newSpots};
+
+        days = [...days.slice(0, pos), day, ...days.slice(pos+1, days.length)]; 
+
+        console.log("Target change", days, "and", day, newSpots);
+
+        return Object.assign({}, state, { days});
+
       default:
         throw new Error(
           `Tried to reduce with unsupported action type: ${action.type}`
@@ -29,52 +44,48 @@ export default function useApplicationData(props) {
     }
   };
   
-
   const [ state, dispatch] = useReducer(reducer, { 
     day: "Monday", days: [], appointments: {}, interviewers: {}
     });
 
-  // const [ stateR, setState] = useState({
-    // day: "Monday",
-  //   days: [],
-  //   appointments: {},
-  //   interviewers: {}
-  // });
   const setDay = day => dispatch({type: "SET_DAY", day});
   const setDays = days => dispatch({type: "SET_APPLICATION_DATA", days});
   const setAppointments = appointments => dispatch({type: "SET_APPLICATION_DATA", appointments});
   const setInterviewer = interviewers => dispatch({type: "SET_APPLICATION_DATA", interviewers});
+  const setSpots = (id, change) =>  dispatch({type: "SET_SPOTS", id, change});
 
-  // const setDaysR = days => setState(prev => ({ ...prev, days}));
-  // const setAppointmentsR = appointments => setState(prev => Object.assign({}, prev, { appointments }));
-  // const setInterviewerR = interviewers => setState(prev => ({ ...prev, interviewers}));
+  //Add an interview to update Database
+  function bookInterview(id, interview) {
+    const change = (state.appointments[id]["interview"] === null ? -1 : 0);
 
-    //Add an interview to update Database
-    function bookInterview(id, interview) {
-      const appointment = {...state.appointments[id], interview: { ...interview }};
-      const appointments = {...state.appointments,[id]: appointment};
+    console.log("Check if null", interview, "to add->", id, "to", state.appointments, "for", change);
+
+    const appointment = {...state.appointments[id], interview: { ...interview }};
+    const appointments = {...state.appointments, [id]: appointment};
+
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, {"interview": interview} )
+      .then(res => {
+        setAppointments(appointments);
+        setSpots( id, change);
+        return Promise.resolve("SHOW"); 
+      })
+      .catch(e => "ERROR_SAVE");
+  }
+    //Cancel an interview to update Database
+    function cancelInterview(id) {
+      const appointment = {...state.appointments[id], interview: null };
+      const appointments = {...state.appointments, [id]: appointment};
   
       return axios
-        .put(`http://localhost:8001/api/appointments/${id}`, {"interview": interview} )
+        .delete(`http://localhost:8001/api/appointments/${id}`, {"interview": null} )
         .then(res => {
           setAppointments(appointments);
-          return Promise.resolve("SHOW"); 
+          setSpots( id, 1);
+          return Promise.resolve("EMPTY"); 
         })
-        .catch(e => "ERROR_SAVE");
+        .catch(e => "ERROR_DELETE");
     }
-      //Cancel an interview to update Database
-      function cancelInterview(id) {
-        const appointment = {...state.appointments[id], interview: null };
-        const appointments = {...state.appointments,[id]: appointment};
-    
-        return axios
-          .delete(`http://localhost:8001/api/appointments/${id}`, {"interview": null} )
-          .then(res => {
-            setAppointments(appointments);
-            return Promise.resolve("EMPTY"); 
-          })
-          .catch(e => "ERROR_DELETE");
-      }
 
     //initial setup of in-memory data
     useEffect(() => {
