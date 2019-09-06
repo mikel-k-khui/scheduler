871943862f908a1b18545f96917f8eefb8e7579a
Mike Chui <mikel.k.khui@gmail.com>
2019-09-06 02:08:28 -0600

    Fixed byproduct with displaying interviewer of an appointment as selected

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,8 +11,8 @@ export default function InterviewerList(props) {
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.value}
-      setInterviewer={(event) => props.onChange(interviewer.id)}
+      selected={interviewer === props.value}
+      setInterviewer={(event) => props.onChange(interviewer)}
     />
     );
   });
