d5a918c7502ef1201d13b4d97059b79a2e59c8bf
Mike Chui <mikel.k.khui@gmail.com>
2019-09-10 12:53:07 -0600

    Completed the testing on reducers functionality and migrated the reducer function to a separate file.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,42 +2,8 @@ import { useEffect, useReducer } from "react";
 import axios from "axios";
 
 import "components/Application.scss";
-
+import reducer, { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "reducers/application";
 const queries = ['/api/days', '/api/appointments', '/api/interviewers'];
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_SPOTS = "SET_SPOTS";
-
-function reducer(state, {type, ...payload}) {
-  // console.log("In Reducer: adding", payload, "to", state);
-  switch (type) {
-    case SET_DAY:
-      return Object.assign({}, state, { day: payload.day});
-    case SET_APPLICATION_DATA:
-        let { ...any} = payload;
-        return Object.assign({}, state, { ...any});
-    case SET_SPOTS: 
-      let { id, change} = payload;
-      // console.log("In set spots:", id, "and change:", change);
-      // console.log("Which day to change:", state.days[Math.floor(id / 5)]);
-
-      const pos = Math.floor(id / 5);
-      let days = [...state["days"]];
-      let day = {...days[pos]};
-      if (day["appointments"].includes(id)) {
-        const newSpots = Number(days[pos]["spots"]) + Number(change);
-        day = {...day, spots: newSpots};
-        days = [...days.slice(0, pos), day, ...days.slice(pos+1, days.length)]; 
-        // console.log("Target change", days, "\nand", day, newSpots);
-      }
-      return Object.assign({}, state, { days });
-
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${type}`
-      );
-  }
-};
 
 export default function useApplicationData(props) {
   
