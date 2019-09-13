ec8639a5e7f87e39ec1e25b8228905102bbe2f00
Mike Chui <mikel.k.khui@gmail.com>
2019-09-13 17:06:10 -0600

    Fixed comments on reducer

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -12,14 +12,15 @@ export default function reducer(state, {type, ...payload}) {
         return Object.assign({}, state, { ...any});
     case SET_SPOTS: 
       let { id, change} = payload;
-      const pos = Math.floor(id / 5);
+      // const dayOf = Math.floor(id / 5);
+      const dayOf = state.days.filter(day => day["appointments"].includes(id))[0]["id"] - 1;
       let days = [...state["days"]];
-      let day = {...days[pos]};
+      let day = {...days[dayOf]};
 
       if (day["appointments"].includes(id)) {
-        const newSpots = Number(days[pos]["spots"]) + Number(change);
+        const newSpots = Number(days[dayOf]["spots"]) + Number(change);
         day = {...day, spots: newSpots};
-        days = [...days.slice(0, pos), day, ...days.slice(pos+1, days.length)];
+        days = [...days.slice(0, dayOf), day, ...days.slice(dayOf+1, days.length)];
       }
     return Object.assign({}, state, { days });
 
